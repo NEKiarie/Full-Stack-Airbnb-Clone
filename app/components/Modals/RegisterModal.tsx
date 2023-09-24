6b8fc9eccca05af62560fe13c96368fff08ba01f
NEKiarie <s49188705@gmail.com>
2023-09-25 01:30:27 +0300

    :sparkles:Added RegisterModal

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -7,11 +7,14 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../Inputs/Input";
 
-export default function RegisterModal() {
+const RegisterModal = () => {
   const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
 
+  //Form Control function
   const {
     register,
     handleSubmit,
@@ -24,6 +27,7 @@ export default function RegisterModal() {
     },
   });
 
+  //OnSubmit handler function
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
@@ -40,9 +44,48 @@ export default function RegisterModal() {
       });
   };
 
-  return;
-  <>
-    {/* <Modal disabled={isLoading}
-   />; */}
-  </>;
-}
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create An Account!" center />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModal;
