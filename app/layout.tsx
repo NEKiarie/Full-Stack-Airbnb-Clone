dea9f7a820d7174c8aa5e5a5ae07b27be5c731e1
NEKiarie <s49188705@gmail.com>
2023-09-25 08:51:21 +0300

    :sparkles:Register UI complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/Modals/RegisterModal";
+import TosterProvider from "./providers/TosterProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <TosterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
